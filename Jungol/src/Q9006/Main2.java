0c66a797de613b22f28e1893565f898677471f9c
DongKyu Ryu <fbehdrb97@naver.com>
2023-09-20 14:17:39 +0900

    Q9006 문제 풀이

---
+++ Jungol/src/Q9006/Main2.java
@@ -0,0 +1,11 @@
+package Q9006;
+
+public class Main2 {
+	public static void main(String[] args) {
+		int a = 123;
+		double  b = 6.500000;
+		System.out.println("수를 출력할 때는 따옴표를 생략합니다.");
+		System.out.println(a);
+		System.out.println(String.format("%.6f",b));
+	}
+}

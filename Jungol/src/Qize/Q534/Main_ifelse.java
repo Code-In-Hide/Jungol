b4a96fb64ef26a2524ec34b17c6fdc17446cdbe4
DongKyu Ryu <fbehdrb97@naver.com>
2023-09-21 17:07:03 +0900

    ''char 문제풀이 + ""String 문제풀이

--- Jungol/src/Qize/Q534/Main_ifelse.java
+++ Jungol/src/Qize/Q534/Main_ifelse.java
@@ -4,14 +4,36 @@ import java.util.Scanner;
 
 public class Main_ifelse {
 	public static void main(String[] args) {
-		Scanner a = new Scanner(System.in);
-		String b = a.next();
+		
+		//'' 로 문제 풀이
+		/*Scanner a = new Scanner(System.in);
+		char b = a.next().charAt(0);
+		a.close();
 
-		if ("A".equals(b)) {System.out.println("Excellent");} 
-		else if ("B".equals(b)) {System.out.println("Good");}
-		else if ("C".equals(b)) {System.out.println("Usually");}
-		else if ("D".equals(b)) {System.out.println("Effort");}
-		else if ("F".equals(b)) {System.out.println("Failure");}
-		else System.out.println("error");
+		if (b == 'A') {System.out.println("Excellent");} 
+		else if (b == 'B') {System.out.println("Good");}
+		else if (b == 'C') {System.out.println("Usually");}
+		else if (b == 'D') {System.out.println("Effort");}
+		else if (b == 'F') {System.out.println("Failure");}
+		else System.out.println("error"); */
+		
+		//"" 로 문제 풀이
+		Scanner a = new Scanner(System.in);
+		String b= a.next();
+		a.close();
+		
+		System.out.println(b);
+		if(b.equals("A")) {
+			System.out.println("Excellent");
+		}else if (b.equals("B")) {
+			System.out.println("Good");
+		}else if (b.equals("C")) {
+			System.out.println("Usually");
+		}else if (b.equals("D")) {
+			System.out.println("Effort");
+		}else if (b.equals("F")) {
+			System.out.println("Failure");
+		}else 
+			System.out.println("error");
 	}
 }

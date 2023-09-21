a69f7b5c66522afd8c5728dcfae3b047d10c5b9b
DongKyu Ryu <fbehdrb97@naver.com>
2023-09-21 12:19:04 +0900

    문자열 1 - 자가진단2 문제풀이

---
+++ Jungol/src/Q594/Main.java
@@ -0,0 +1,11 @@
+package Q594;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner a = new Scanner(System.in);
+		String b = a.next();
+		System.out.println(b + b);
+	}
+}

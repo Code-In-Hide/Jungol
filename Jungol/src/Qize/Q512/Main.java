24fb7c87d8faf96c31099d8402c065d0ecdc254a
DongKyu Ryu <fbehdrb97@naver.com>
2023-09-22 22:38:53 +0900

    printf를 활용한 소수점 표현법

---
+++ Jungol/src/Qize/Q512/Main.java
@@ -0,0 +1,11 @@
+package Qize.Q512;
+
+public class Main {
+	public static void main(String[] args) {
+		int a = 49;
+		double b = 0.268300;
+		double arg = a * b;
+
+		System.out.printf("%d * %f = %.6f", a, b, arg);
+	}
+}

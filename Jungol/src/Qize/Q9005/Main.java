6f54ea13e40344eb801b57012afb52db8bc5ea1c
DongKyu Ryu <fbehdrb97@naver.com>
2023-09-22 13:32:56 +0900

    서식 문자열 사용 법

---
+++ Jungol/src/Qize/Q9005/Main.java
@@ -0,0 +1,10 @@
+package Qize.Q9005;
+
+public class Main {
+	public static void main(String[] args) {
+		String a = "이것은 문자열을 나타냅니다.";
+		String b = "서식 문자로 출력하는 방법입니다.";
+        System.out.printf("%s\n",b);
+        System.out.printf("%s",a);
+    }
+}

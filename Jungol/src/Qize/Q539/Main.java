8162e11e7060528549d4212f71d7d527b0e285b1
DongKyu Ryu <fbehdrb97@naver.com>
2023-09-22 12:30:50 +0900

    정수를 계속 입력받다가 100 이상의 수가 입력이 되면 마지막 입력된 수를 포함하여 합계와 평균을 출력하는 프로그램을 작성하시오.
    (평균은 반올림하여 소수 첫째자리까지 출력한다.)
    
    문제 풀이

---
+++ Jungol/src/Qize/Q539/Main.java
@@ -0,0 +1,27 @@
+package Qize.Q539;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int sum = 0;
+		int cnt = 0;
+
+		while (true) {
+			int inp = sc.nextInt();
+
+			if (inp < 100) {
+				sum += inp;
+				cnt++;
+			} else if (inp >= 100) {
+				sum += inp;
+				cnt++;
+				System.out.println(sum);
+				System.out.printf("%.1f\n", (double)sum / cnt);
+				break;
+			}
+		}
+		sc.close();
+	}
+}

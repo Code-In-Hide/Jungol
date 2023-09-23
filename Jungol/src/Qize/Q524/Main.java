ec61b773759df3df44a6628d68a896d2f2ed7c6f
DongKyu Ryu <fbehdrb97@naver.com>
2023-09-23 17:55:30 +0900

    boolean을 적용한후 논리곱,논리합을 사용해서 출력

---
+++ Jungol/src/Qize/Q524/Main.java
@@ -0,0 +1,16 @@
+package Qize.Q524;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		boolean c = (a != 0);
+		boolean d = (b != 0);
+		
+		System.out.println((c && d) + " " + (c || d));
+	}
+}

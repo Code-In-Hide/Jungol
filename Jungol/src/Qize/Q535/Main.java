7110ece35a012a691a509a3a88a94f9e676f02a3
DongKyu Ryu <fbehdrb97@naver.com>
2023-09-25 13:56:01 +0900

    switch 를활용한 조건문

--- Jungol/src/Qize/Q535/Main.java
+++ Jungol/src/Qize/Q535/Main.java
@@ -18,8 +18,6 @@ public class Main {
 			System.out.println("seasonal semester");
 			break;
 		case 1:
-			System.out.println("retake");
-			break;
 		case 0:
 			System.out.println("retake");
 			break;

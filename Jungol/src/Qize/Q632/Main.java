90e3a5c6e4f0022ca285a4902662457214846773
DongKyu Ryu <fbehdrb97@naver.com>
2023-09-26 13:58:21 +0900

    선택 제어문

---
+++ Jungol/src/Qize/Q632/Main.java
@@ -0,0 +1,21 @@
+package Qize.Q632;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		sc.close();
+
+		if (a < b && a < c) {
+			System.out.println(a);
+		} else if (b < c) {
+			System.out.println(b);
+		} else {
+			System.out.println(c);
+		}
+	}
+}

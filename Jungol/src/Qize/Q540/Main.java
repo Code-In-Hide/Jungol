3e12d83b4d994bcc0739f98cd6ccc75c657bc26a
DongKyu Ryu <fbehdrb97@naver.com>
2023-09-27 17:31:20 +0900

    반복제어문 3의 배수는 나눈값 출력 아니면 넘김

---
+++ Jungol/src/Qize/Q540/Main.java
@@ -0,0 +1,19 @@
+package Qize.Q540;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+			int a = sc.nextInt();
+			if (a % 3 == 0) {
+				System.out.println(a / 3);
+			} else if (a == -1) {
+				break;
+			}
+		}
+		sc.close();
+	}
+}

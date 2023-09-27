a9f76e7a1dd388a087a19a32ff7900d69b6ae1e7
DongKyu Ryu <fbehdrb97@naver.com>
2023-09-27 13:58:46 +0900

    함수 메소드를 이용한 출력방법

---
+++ Jungol/src/Qize/Q571/Main.java
@@ -0,0 +1,18 @@
+package Qize.Q571;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		sc.close();
+
+		for (int i = 0; i < a; i++) {
+			print();
+		}
+	}
+	static void print() {
+		System.out.println("~!@#$^&*()_+|");
+	}
+}

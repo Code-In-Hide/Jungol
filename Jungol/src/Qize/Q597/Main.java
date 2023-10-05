0994a7d15468726209998ebb1b9ce2e87c9481d9
DongKyu Ryu <fbehdrb97@naver.com>
2023-10-05 13:37:25 +0900

    문자열1(문자열 개수출력후 문자열개수합계출력)

---
+++ Jungol/src/Qize/Q597/Main.java
@@ -0,0 +1,16 @@
+package Qize.Q597;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str1 = sc.nextLine();
+		String str2 = sc.nextLine();
+		
+		int a = str1.length();
+		int b = str2.length();
+		
+		System.out.println(a + b);
+	}
+}
